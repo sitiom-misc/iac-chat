8ee0a94f5e455bfc91b89f18b597800006c8b943
Ryan Caezar Itang <sitiom@proton.me>
2023-11-22 00:53:50 +0800

    feat: add contacts via email

--- src/app/(app)/(tabs)/contacts.tsx
+++ src/app/(app)/(tabs)/contacts.tsx
@@ -1,27 +1,44 @@
 import { Room, User } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { router } from "expo-router";
 import {
   CollectionReference,
   DocumentReference,
-  and,
   collection,
   doc,
   getDocs,
+  limit,
   orderBy,
   query,
   serverTimestamp,
   setDoc,
+  updateDoc,
   where,
 } from "firebase/firestore";
-import { View, StyleSheet, FlatList, ToastAndroid } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { View, StyleSheet, FlatList } from "react-native";
 import {
   ActivityIndicator,
   Avatar,
+  Button,
+  Dialog,
   FAB,
+  HelperText,
+  Portal,
   Text,
+  TextInput,
   TouchableRipple,
 } from "react-native-paper";
-import { useAuth, useFirestore, useFirestoreDocData, useUser } from "reactfire";
+import {
+  useAuth,
+  useFirestore,
+  useFirestoreCollectionData,
+  useFirestoreDocData,
+} from "reactfire";
+import ConfettiCannon from "react-native-confetti-cannon";
+import { z } from "zod";
+import Explosion from "react-native-confetti-cannon";
 
 export default function ContactScreen() {
   const { currentUser } = useAuth();
@@ -41,6 +58,59 @@ export default function ContactScreen() {
       idField: "id",
     }
   );
+  const [isDialogvisible, setDialogVisible] = useState(false);
+  const [isConfettiVisible, setConfettiVisible] = useState(false);
+
+  const validUserSchema = z.object({
+    email: z
+      .string()
+      .email()
+      .refine(
+        (email) => email !== currentUser.email,
+        "You cannot add yourself as a contact"
+      )
+      .refine(async (email) => {
+        const userQuery = query(
+          collection(firestore, "users") as CollectionReference<User>,
+          where("email", "==", email),
+          limit(1)
+        );
+        const userSnap = await getDocs(userQuery);
+        return !userSnap.empty;
+      }, "User does not exist")
+      .refine(async (email) => {
+        const userQuery = query(
+          collection(firestore, "users") as CollectionReference<User>,
+          where("email", "==", email),
+          limit(1)
+        );
+        const userSnap = await getDocs(userQuery);
+        if (userSnap.empty) {
+          return false;
+        }
+        const userId = userSnap.docs[0].id ?? "";
+        return !userData.contacts.includes(userId);
+      }, "User is already in your contacts"),
+  });
+
+  const {
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors, isSubmitting, isValid, isSubmitSuccessful },
+    reset,
+  } = useForm<z.infer<typeof validUserSchema>>({
+    resolver: zodResolver(validUserSchema),
+    mode: "onChange",
+  });
+  const email = watch("email");
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful]);
+
   if (userDataStatus === "loading") {
     return <ActivityIndicator />;
   }
@@ -48,8 +118,121 @@ export default function ContactScreen() {
     return null;
   }
 
+  const UserProfile = ({ email }: { email: string }) => {
+    const userQuery = query(
+      collection(firestore, "users") as CollectionReference<User>,
+      where("email", "==", email),
+      limit(1)
+    );
+
+    const { status: userStatus, data: userResults } =
+      useFirestoreCollectionData(userQuery, {
+        idField: "id",
+      });
+    if (userStatus === "loading") {
+      return <ActivityIndicator />;
+    }
+    if (!userResults || userResults.length === 0) {
+      return null;
+    }
+
+    const user = userResults[0];
+
+    return (
+      <>
+        <Avatar.Image
+          size={60}
+          source={
+            user.avatarUrl
+              ? { uri: user.avatarUrl }
+              : require("@/assets/images/avatar-2.png")
+          }
+        />
+        <Text variant="bodyLarge">{user.name}</Text>
+      </>
+    );
+  };
+
   return (
     <>
+      {isConfettiVisible && (
+        <ConfettiCannon
+          count={100}
+          origin={{ x: -10, y: 0 }}
+          fadeOut={true}
+          onAnimationEnd={() => setConfettiVisible(false)}
+        />
+      )}
+      <Portal>
+        <Dialog
+          visible={isDialogvisible}
+          onDismiss={() => {
+            setDialogVisible(false);
+            reset();
+          }}
+        >
+          <Dialog.Title style={{ textAlign: "center" }}>
+            Add Contact
+          </Dialog.Title>
+          <Dialog.Content>
+            <View style={{ alignItems: "center", margin: 20, gap: 8 }}>
+              {(email && !errors.email) ||
+              errors.email?.message === "User is already in your contacts" ||
+              errors.email?.message ===
+                "You cannot add yourself as a contact" ? (
+                <UserProfile email={email} />
+              ) : (
+                <Avatar.Image
+                  size={60}
+                  source={require("@/assets/images/avatar-2.png")}
+                />
+              )}
+            </View>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  label="Email"
+                  onBlur={onBlur}
+                  keyboardType="email-address"
+                  onChangeText={onChange}
+                  value={value}
+                  error={!!errors.email}
+                />
+              )}
+              name="email"
+            />
+            <HelperText type="error" visible={!!errors.email}>
+              {errors.email && errors.email.message}
+            </HelperText>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={handleSubmit(async ({ email }) => {
+                // Add user to contacts
+                const userQuery = query(
+                  collection(firestore, "users") as CollectionReference<User>,
+                  where("email", "==", email),
+                  limit(1)
+                );
+                const userSnap = await getDocs(userQuery);
+                if (userSnap.empty) {
+                  return;
+                }
+                const userId = userSnap.docs[0].id ?? "";
+                await updateDoc(userDoc, {
+                  contacts: [...userData.contacts, userId],
+                });
+                setDialogVisible(false);
+                setConfettiVisible(true);
+              })}
+              disabled={!isValid || isSubmitting}
+            >
+              Done
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
       <FlatList
         style={styles.container}
         data={userData.contacts}
@@ -59,7 +242,7 @@ export default function ContactScreen() {
       <FAB
         icon="plus"
         style={styles.fab}
-        onPress={() => console.log("Pressed")}
+        onPress={() => setDialogVisible(true)}
       />
     </>
   );
